c106d62508622f88dd204f450bad410ad219c563
KagazzIe <joeraccioiv@gmail.com>
2019-01-17 16:11:01 -0500

    Bug fixes and formatting BuildingGroup

--- Kookie_Klacker2.py
+++ Kookie_Klacker2.py
@@ -27,59 +27,27 @@ class Player:
         }#All building types : how many the player owns
         
         
-"""
     def buy_building(self, building: 'BuildingGroup', amount: int = 1):
-        
-        price = math.floor(building.base_cost*(building_cost_multiplyer**(self.owned_buildings[building]+amount) - building_cost_multiplyer**(self.owned_buildings[building]))/.15)+1
-        
-        if building.can_buy == False:
-            print(building.name)
-            print("amount :", amount)
-            raise Exception("You do not have the cookies to buy this")
-        print("Bought", amount, building.name)
-
-        self.cookies -= price
-        self.owned_buildings[building] += amount
-        building.cps_total = self.owned_buildings[building]*building.cps_per
-        building.next_cost = math.floor(building.base_cost*(building_cost_multiplyer**self.owned_buildings[building]))+1
-
-        self.update_cps()
-        self.check_buy()   
-  """
+        pass
+        #TODO
      
-"""
+
     def sell_building(self, building: 'BuildingGroup', amount: int = 1):
-        if self.owned_buildings[building] < amount:
-            raise Exception("You are trying to sell more building than you have")
-        
-        print("Sold", amount, building.name)
+        pass
+        #TODO
 
-        self.owned_buildings[building] -= amount
-        self.cookies += math.floor(building.next_cost/(building_sell_multiplyer**amount))
-
-        if self.owned_buildings[building] != 0:
-            building.next_cost = math.floor(building.base_cost*(building_cost_multiplyer**self.owned_buildings[building]))+1
-        else:
-            building.next_cost = building.base_cost
-        building.cps_total = self.owned_buildings[building]*building.cps_per
-
-        self.update_cps()
-        self.check_buy()
-"""
     def building_count(self, building: 'BuildingGroup') -> 'BuildingGroup':
         return self.owned_buildings[building]
+        
     def update_cps(self):
-        total = 0
-        for building_type in self.owned_buildings.keys():
-            total += self.owned_buildings[building_type]*building_type.cps_per
-        self.total_cps = total
+        pass #TODO
 
     def stats(self):
-        pass
+        pass#TODO
 
 
     def check_buy(self):
-        pass
+        pass#TODO
 
 
 
@@ -105,36 +73,51 @@ class BuildingGroup(ABC):
 
     @property
     def next_cost(self):
-        return next_cost = math.ceil(self.base_cost*(self.building_cost_multiplyer**self.player.owned_buildings[building]))
+        #return math.ceil(self.base_cost*(self.building_cost_multiplyer**self.player.owned_buildings[building]))
+        pass #TODO
 
     @abstractmethod
     def stats(self):
         #TODO
         pass
     
-    def upgrade():
+    def upgrade(self):
         self.upgrades.next()
-        self.player
+        self._player
         
 
 class Grandma(BuildingGroup):
-    def __init__(self, player: Player):
+    def __init__(self, 
+        player: Player,
+        name: str,
+        base_cost: int,
+        cps: int
+        ):
+        
         BuildingGroup.__init__(self, player)
+        self. name = name
+        self._player = player
+
+        self.base_cost = base_cost
+        self.next_cost = base_cost
+        
+        self.base_cps = cps
+        self.cps_mult = 1
 
 
     @property
     def base_cps(self):
         return 1
 
-    def cps_func(player) -> 'cps':
-    return player.count(Grandma) * 4
+    def cps_func(self,player) -> 'cps':
+        return player.count(Grandma) * 4
 
 
 class PathedUpgrades:
     def __init__(
             self, 
             name, 
-            cps_func 
+            cps_func,
             base_price: int, 
             reqs = [1,5,25,50,100,150,200,250,300,350,400], 
             mults=[5,10,100,100,100,1000,1000,1000,1000,10000]):
@@ -143,7 +126,7 @@ class PathedUpgrades:
         self.mults = mults
         self.base_price = base_price
         self.price = base_price
-        self.cps_func
+        self.cps_func = cps_func
 
 
     @property
@@ -152,13 +135,13 @@ class PathedUpgrades:
 
     @property
     def next_mult(self):
-        return self.mults[0] self.mults else None
+        return self.mults[0] if self.mults else None
 
     def next(self):
         self.reqs.pop()
         self.mults.pop()
 
-    def cps(player):
+    def cps(self, player):
         self.cps_func(player)        
 
         
