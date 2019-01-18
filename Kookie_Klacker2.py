bf982c98d8e901cfacf8b9dca1d71396a7b915d2
KagazzIe <joeraccioiv@gmail.com>
2019-01-18 11:50:39 -0500

    All Buildings Completly Implemented. Next is Player Methods

--- Kookie_Klacker2.py
+++ Kookie_Klacker2.py
@@ -10,17 +10,49 @@ building_sell_multiplyer = 4
 
 class Player:
     def __init__(self, name: str):
-        
+
         self.cookies = 2080
-        self.total_cps = 0
         #self.strategy = ai
 
         self.name = name
 
-        self.owned_buildings = {
-
-        }#All building types : how many the player owns
-        
+        self.owned_buildings = self.create_BuildingGroups()
+    
+    def create_BuildingGroups(self):
+        cursor = Cursor(self, 'Cursor', 15, 0.1)
+        grandma = DefaultBuilding(self, 'Grandma', 100.0, 1.0)
+        farm = DefaultBuilding(self, 'Farm', 1100.0, 8.0)
+        mine = DefaultBuilding(self, 'Mine', 12000.0, 47.0)
+        factory = DefaultBuilding(self, 'Factory', 130000.0, 260.0)
+        bank = DefaultBuilding(self, 'Bank', 1400000.0, 1400.0)
+        temple = DefaultBuilding(self, 'Temple', 20000000.0, 7800.0)
+        wizard_tower = DefaultBuilding(self, 'Wizard Tower', 330000000.0, 44000.0)
+        shipment = DefaultBuilding(self, 'Shipment', 5100000000.0, 260000.0)
+        alchemy_lab = DefaultBuilding(self, 'Alchemy Lab', 75000000000.0, 1600000.0)
+        portal = DefaultBuilding(self, 'Portal', 1000000000000.0, 10000000.0)
+        time_machine = DefaultBuilding(self, 'Time Machine', 14000000000000.0, 65000000.0)
+        antimatter_condenser = DefaultBuilding(self, 'Anitmatter Condenser', 170000000000000.0, 430000000.0)
+        prism = DefaultBuilding(self, 'Prism', 2100000000000000.0, 2900000000.0)
+        chance_maker = DefaultBuilding(self, 'Chance Maker', 26000000000000000.0, 21000000000.0)
+        fractal_engine = DefaultBuilding(self, 'Fractal Engine', 310000000000000000.0, 150000000000.0)
+        return {
+            cursor: 0,
+            grandma: 0,
+            farm: 0,
+            mine: 0,
+            factory: 0,
+            bank: 0,
+            temple: 0,
+            wizard_tower: 0,
+            shipment: 0,
+            alchemy_lab: 0,
+            portal: 0,
+            time_machine: 0,
+            antimatter_condenser: 0,
+            prism: 0,
+            chance_maker: 0,
+            fractal_engine: 0
+        }
         
     def buy_building(self, building: 'BuildingGroup', amount: int = 1):
         pass
@@ -33,9 +65,12 @@ class Player:
 
     def building_count(self, building: 'BuildingGroup') -> 'BuildingGroup':
         return self.owned_buildings[building]
-        
-    def update_cps(self):
-        pass #TODO
+    
+    @property
+    def total_cps(self):
+        for building in self.owned_buildings:
+            cps += self.count(building)*building.cps_per
+        return cps
 
     def stats(self):
         pass#TODO
@@ -68,9 +103,9 @@ class BuildingGroup(ABC):
 
         self.upgrades = upgrades
 
-    @abstractmethod
-    def cps_per(self):
-        raise NotImplementedError()
+    @property
+    def cps_per(self) -> int:
+        return self.base_cost*(2**self.upgrade_count)
 
     @property
     def next_cost(self):
@@ -84,46 +119,75 @@ class BuildingGroup(ABC):
     def next_mult(self):
         return self.upgrades.mults[0] if self.upgrades.mults else None
 
-
-    @abstractmethod
-    def stats(self):
-        raise NotImplementedError()
-        
-
-class Grandma(BuildingGroup):
-    def __init__(self, 
-        player: Player,
-        name: str,
-        base_cost: int,
-        cps: int,
-        ):
-        
-        upgrades = PathedUpgrades(name+'Upgrades', 1100)
-        BuildingGroup.__init__(self, player, name, base_cost, cps, upgrades)
-        
-        self.name = name
-
-    def cps_per(self) -> int:
-        return self.base_cost*(2**self.upgrade_count)
-    
     def upgrade(self):
         self.upgrades.next()
         self._player
-
-
     
     def stats(self):
         print(self.name)
         print("Base Cost:",self.base_cost)
         print("Upgrade Count:",self.upgrade_count)
         print("Base Cps:",self.base_cps)
-        print("Cps Per:", self.cps_per(self))
-        print("Next Cost:", self.next_cost(self))
-        print("Next Upgrade Req:", self.next_req(self))
-        print("Next Cost Multiplyer:", self.next_mult(self))
+        print("Cps Per:", self.cps_per)
+        print("Next Cost:", self.next_cost)
         print("Base Upgrade Price:",self.upgrades.base_price)
+        print("Next Cost Multiplyer:", self.next_mult)
+        print("Next Upgrade Req:", self.next_req)
+        
+        
+
+class DefaultBuilding(BuildingGroup):
+    def __init__(self, 
+        player: Player,
+        name: str,
+        base_cost: int,
+        cps: int,
+        base_upgrade_cost_mult: int = 10
+        ):
+        
+        upgrades = PathedUpgrades(name+'Upgrades', base_cost*base_upgrade_cost_mult)
+        BuildingGroup.__init__(self, player, name, base_cost, cps, upgrades)
+
+class Cursor(BuildingGroup):
+    def __init__(self, 
+        player: Player,
+        name: str,
+        base_cost: int,
+        cps: int,
+        base_upgrade_cost: int = 100
+        ):
+        
+        upgrades = PathedUpgrades(
+        name+'Upgrades',
+        base_upgrade_cost,
+        [1,1,10,25,50,100,150,200,250,300,350, 400],
+        [5, 100, 1000, 100000, 1000000, 10000000, 100000000, 100000000000, 100000000000000, 100000000000000000, 100000000000000000000]
+        )
+        BuildingGroup.__init__(self, player, name, base_cost, cps, upgrades)
+    
+    @property
+    def cps_per(self):
+        upgrades_till_add = 3
+        if upgrades_till_add >= self.upgrade_count:
+            cps = self.base_cps*(2**self.upgrade_count)
+        else:
+            cps = self.base_cps*(2**upgrades_till_add)
+
+            total_buildings = 0
+
+            cps_add = 0
+            cps_add_constants = (0.1, 0.5, 5, 50, 500, 5000, 50000, 500000, 5000000)
+            for i in range(self.upgrade_count-upgrades_till_add):
+                cps_add += cps_add_constants[i]
+            
+            for building in self._player.owned_buildings:
+                if building != Cursor:
+                    total_buildings += self._player.owned_buildings[building]
+            cps += total_buildings*cps_add
+        return cps
 
 
+    
 
 class PathedUpgrades:
     def __init__(
@@ -151,7 +215,3 @@ class PathedUpgrades:
 
 
 p = Player('Joe')
-g = Grandma(p, 'Grandma', 100, 1)
-g.stats()
-
-
