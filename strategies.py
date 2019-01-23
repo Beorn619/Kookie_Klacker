1d66b567fe20808b488920318aec8a91efcf088f
KagazzIe <joeraccioiv@gmail.com>
2019-01-23 18:42:48 -0500

    Re-doing the framework

--- strategies.py
+++ strategies.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-import math, time
+import math, operator
 
 
 class Strategy(ABC):
@@ -11,61 +11,73 @@ class KagazzieAI(Strategy):
     def __init__(self, player):
         self.player = player
         self.target = None
+        self.target_type = None
 
     def update(self):
-        if self.target == None:
-            buildings_time_to_profit, upgrades_time_to_profit = self.times_to_profit()
-            self.target = self.choose_target(buildings_time_to_profit, upgrades_time_to_profit)
-        if (self.player.tick % 10) == 0:
-            self.player.stats()
-        self.target = self.attempt_to_buy(self.target)
 
+        if self.target == None:
+            self.target, self.target_type = self.choose_target()
+
+        self.attempt_buy()
+
+        if self.player.tick % 500 == 0:
+            self.player.stats()
+            
     
     def time_to_buy(self, obj):
         time = math.ceil((obj.next_cost-self.player.cookies)/self.player.total_cps)
         return time
     
-    def time_to_repay_building(self, building):
-        return (building.next_cost)/building.cps_per()
+    def building_roi(self, obj):
+        return obj.next_cost/obj.cps_per()
     
-    def times_to_profit(self):
-        buildings_time_to_profit = []
-        upgrades_time_to_profit = []
-        for building in self.player.owned_buildings.keys():
-            buildings_time_to_profit.append(math.ceil(self.time_to_buy(building)+self.time_to_repay_building(building)))
+    def upgrade_roi(self, obj):
+        return (obj.cps_per(1) - obj.cps_per())/obj.upgrades.next_cost
+    
+    def grandma_upgrade_roi(self, obj):
+        pass
+    
+    def income_upgrade_roi(self, obj):
+        pass
+
+    def choose_target(self):
+        building_times = []
+        upgrade_times = []
+        #grandma_upgrade_times = []
+        #income_upgrade_times = []
+
+        purchases = [building_times, upgrade_times]
+
+        for building in self.player.buildings:
+            building_times.append([math.ceil(self.time_to_buy(building)+self.building_roi(building)), building, 'Building'])
             if building.upgrades.next_req <= self.player.owned_buildings[building]:
-                upgrades_time_to_profit.append(self.time_to_buy(building.upgrades)+self.time_to_repay_upgrade(building))
+                upgrade_times.append([math.ceil(self.time_to_buy(building.upgrades)+self.upgrade_roi(building)), building, 'Upgrade'])
             else:
-                upgrades_time_to_profit.append(None)
-            
-        return buildings_time_to_profit, upgrades_time_to_profit
-    
-    def time_to_repay_upgrade(self, building):
-        return int(math.ceil((building.upgrades.next_cost)/((building.cps_per(1)-building.cps_per())*self.player.owned_buildings[building])))
-    
-    def choose_target(self, buildings_time_to_profit, upgrades_time_to_profit):
-        target_time = buildings_time_to_profit[0]
-        target = [self.player.buildings[0],'building']
-        for i in range(0,len(self.player.buildings)):
-            if buildings_time_to_profit[i] < target_time:
-                target = [self.player.buildings[i], 'building']
-                target_time = buildings_time_to_profit[i]
-            if upgrades_time_to_profit[i]:
-                if upgrades_time_to_profit[i] < target_time:
-                    target = [self.player.buildings[i], 'upgrade']
-                    target_time = buildings_time_to_profit[i]
-        return target
+                pass#Make this calculate ROI on the Quickest path to upgrade TODO
+        for group in purchases:
+            group.sort(key=lambda tup: tup[0])
+        
+        target = purchases[0][0][1]
+        target_time = purchases[0][0][0]
+        target_type = purchases[0][0][2]
+        for group in purchases:
+            if len(group) == 0:
+                continue
+            elif group[0][0] < target_time:
+                target = group[0][1]
+                target_time = group[0][0]
+                target_type = group[0][2]
+        return target, target_type
 
-    def attempt_to_buy(self, target):
-        obj = target[0]
-        t = target[1]
-
-        if t == 'building':
-            if obj.can_buy:
-                self.player.buy_building(self.target[0])
-                return None
-        else:
-            if obj.can_upgrade:
-                self.player.upgrade_building(self.target[0])
-                return None
-        return target
+    def attempt_buy(self):
+        if self.target_type == 'Building':
+            if self.target.can_buy == True:
+                self.player.buy_building(self.target)
+                self.target = None
+                self.target_type = None
+        
+        elif self.target_type == 'Upgrade':
+            if self.target.can_upgrade:
+                self.player.upgrade_building(self.target)
+                self.target = None
+                self.target_type = None
